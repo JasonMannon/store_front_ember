30a97f2fc011a3401dc06e6cfc410c674a0747a4
Jason Mannon <jmannonpdx@gmail.com>
2014-05-01 11:19:44 -0700

    adds index controller

--- js/app.js
+++ js/app.js
@@ -2,10 +2,18 @@ var Store = Ember.Application.create({
   LOG_TRANSITIONS: true
 });
 
+Store.IndexController = Ember.Controller.extend({
+  productsCount: 6,
+  logo: 'images/logo.png',
+  time: function() {
+    return (new Date()).toDateString()
+  }.property()
+});
+
 Store.Router.map(function() {
   this.route('about', { path: '/aboutus' });
 });
 
 Store.Router.map(function() {
-  this.route('credits');
+  this.route('credits', { path: '/thanks' });
 });
